d72038921c7f0ff5d0226d6b55760e5900b36750
Lucas Ferreira <lucasf538@gmail.com>
2022-02-10 16:36:49 -0300

    Projeto que seleciona quartos para pessoas e depois mostra onde cada uma está hospedada

---
+++ Pensonato/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Pensonato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quartos[] q = new Quartos[9];
+            Console.Write("Quantos quartos vão ser separados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                Console.WriteLine("\nRent #"+(i+1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+
+                q[room] = new Quartos(nome, email);
+            }
+
+            Console.WriteLine("\nBusy Rooms: ");
+            foreach (Quartos obj in q)
+            {
+                if (q != null)
+                {
+                    Console.WriteLine(": "+q);
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Pensonato/Quartos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensonato
+{
+    class Quartos
+    {
+        public string Nome;
+        public string Email;
+        
+
+        public Quartos(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+            
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+
+
+
+
+    }
+}
